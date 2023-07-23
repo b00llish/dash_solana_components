9ccb7ee2de2ff8d94a39aa272b0e113df6f8f4cb
b00llish <b00llish@pm.me>
2023-07-23 12:09:16 -0500

    add comments and annotations

--- dash_solana_components/src/ts/components/SolanaWalletMultiButton.tsx
+++ dash_solana_components/src/ts/components/SolanaWalletMultiButton.tsx
@@ -1,3 +1,10 @@
+/**
+ * This file contains a React component for a Solana Wallet Multi Button, which provides
+ * an interface to interact with different Solana wallets and return a public key when connected.
+ * This component uses the '@solana/wallet-adapter-react' library to handle wallet interactions.
+ */
+
+// Import required modules
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui';
@@ -13,9 +20,15 @@ import React, {useState, FC, ReactNode, useEffect, useMemo} from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import {DashComponentProps} from '../props';
 
+// Import required stylesheets  
 require('./SolanaWalletMultiButton.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
 
+/**
+ * Define component properties.
+ * The component receives a network prop which can be one of 'devnet', 'mainnet', 'testnet',
+ * and a publicKeyState prop which is used to store and provide the public key state.
+ */
 type Props = {
     // Insert props
     network: 'devnet' | 'mainnet' | 'testnet';
@@ -24,9 +37,16 @@ type Props = {
 } & DashComponentProps;
 
 /**
- * Component description
+ * SolanaWalletMultiButton is a functional React component that provides an interface to
+ * interact with different Solana wallets.
+ * 
+ * It handles connection, disconnection, and returns the public key of the connected wallet.
+ * 
+ * When connected, the public key is made available through a prop, "publicKeyState".
+ * When disconnected, 'null' is displayed.
  */
 
+// Define network values
 const NETWORKS = {
   'devnet': WalletAdapterNetwork.Devnet,
   'mainnet': WalletAdapterNetwork.Mainnet,
@@ -54,13 +74,17 @@ SolanaWalletMultiButton["defaultProps"] = {network: 'mainnet', PublicKeyState: n
 
 export default SolanaWalletMultiButton;
 
+// The Context component is responsible for providing the necessary wallet connections 
+// and context to its child components (passed as the `children` prop).
 const Context: FC<{ children: ReactNode, network: WalletAdapterNetwork, onPublicKeyUpdate: (publicKey: string | null) => void }> = ({ children, network , onPublicKeyUpdate }) => {
+    
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    // const network = WalletAdapterNetwork.Devnet;
-
     // Can provide a custom RPC endpoint here.
+    // The `useMemo` hook is used to recompute the endpoint only when the network value changes.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
+    // This useMemo defines the array of wallets our app will support.
+    // The array is redefined only when the network changes.
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
@@ -73,30 +97,42 @@ const Context: FC<{ children: ReactNode, network: WalletAdapterNetwork, onPublic
         [network]
     );
 
+    // The ConnectionProvider, WalletProvider, and WalletModalProvider components
+    // are part of the @solana/wallet-adapter-react library. They provide the necessary
+    // context and functionality for the WalletMultiButton and useWallet hook to function.
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets} autoConnect>
                 <WalletModalProvider>
-                <Content onPublicKeyUpdate={onPublicKeyUpdate}/>
-                    </WalletModalProvider>
+                    <Content onPublicKeyUpdate={onPublicKeyUpdate}/>
+                </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
     );
 };
 
-const Content: FC<{ onPublicKeyUpdate: any }> = ({ onPublicKeyUpdate }) => {
+// The Content component renders the WalletMultiButton and also monitors the connection status 
+// and public key of the wallet via the `useWallet` hook.
+const Content: FC<{ onPublicKeyUpdate: (publicKey: string | null) => void }> = ({ onPublicKeyUpdate }) => {
+    
+    // The useWallet hook provides the `publicKey` of the connected wallet and a boolean
+    // `connected` indicating the connection status.
     const { publicKey, connected } = useWallet();
 
+    // This effect runs every time the `connected`, `publicKey`, or `onPublicKeyUpdate` values change.
+    // If a wallet is connected, it converts the public key to a string and passes it to the `onPublicKeyUpdate` function.
     useEffect(() => {
         if (connected) {
-            // If the wallet is connected, `publicKey` will be defined.
-            // You can then pass `publicKey` to a callback function, or use it in some other way.
             const publicKeyString = publicKey?.toString();
             console.log(publicKeyString);
             onPublicKeyUpdate(publicKeyString);
+        } else {
+            onPublicKeyUpdate(null);
         }
     }, [connected, publicKey, onPublicKeyUpdate]);
 
+    // This component renders the WalletMultiButton, which provides the user interface 
+    // for connecting and disconnecting wallets.
     return (
         <div className="SolanaWalletMultiButton">
             <WalletMultiButton />
