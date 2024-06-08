b1a6fe93797eff4ade0055cba32212fad696bd7a
b00llish <b00llish@pm.me>
2024-06-08 11:49:07 -0500

    add conditional logic to prevent unnecessary firing for button wrapper

--- dash_solana_components/src/ts/components/TransactionButtonWrapper.tsx
+++ dash_solana_components/src/ts/components/TransactionButtonWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as web3 from '@solana/web3.js';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { TransactionButtonWrapperProps } from '../props/transactionButtonWrapper';
@@ -20,27 +20,27 @@ const TransactionButtonWrapper = ({
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
     const [loading, setLoading] = useState(false);
-    const [executed, setExecuted] = useState(false);  // Flag to prevent multiple executions
+    const [executed, setExecuted] = useState(false);
+    const prevInstructionsRef = useRef<string[] | null>(null);
 
     useEffect(() => {
+        if (
+            transactionInstructions === prevInstructionsRef.current ||
+            !transactionInstructions ||
+            transactionInstructions.length === 0
+        ) {
+            return;
+        }
+
         const executeTransaction = async () => {
             if (!publicKey || !connection) {
                 console.log('Please connect your wallet.');
                 return;
             }
 
-            if (!transactionInstructions || transactionInstructions.length === 0) {
-                console.log('No instructions provided');
-                return;
-            }
-
-            if (executed) {
-                console.log('Transaction already executed');
-                return;
-            }
-
             setLoading(true);
-            setExecuted(true);  // Set the flag to prevent further executions
+            setExecuted(true);
+            prevInstructionsRef.current = transactionInstructions; // Update the ref to the current instructions
 
             try {
                 const transaction = new web3.Transaction();
@@ -82,11 +82,12 @@ const TransactionButtonWrapper = ({
                 console.error('Transaction failed:', error);
             } finally {
                 setLoading(false);
+                setExecuted(false);
             }
         };
 
         executeTransaction();
-    }, [transactionInstructions, connection, publicKey, sendTransaction, setProps, onTransactionSent, executed]);
+    }, [transactionInstructions, connection, publicKey, sendTransaction, setProps, onTransactionSent]);
 
     const handleClick = () => {
         console.log('Button clicked, waiting for transaction instructions...');
